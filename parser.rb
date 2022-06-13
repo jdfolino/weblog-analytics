0bfd776790b27241074dd1ad34be9506eada4816
Jarrod Folino <jarrod.folino@coderacademy.edu.au>
2022-06-13 15:13:11 +1000

    Added missing require

--- parser.rb
+++ parser.rb
@@ -3,6 +3,7 @@
 
 require_relative 'lib/runner'
 require_relative 'lib/io/command_parser'
+require_relative 'lib/app_logger'
 AppLogger.instance.level = ENV['LOG_LEVEL'] || 3
 options = CommandParser.parse(ARGV)
 Runner.new(options).run! unless options[:help]
